a69523a5aab7554dfc21949fefe2ad5124ff704e
Rajesh MR <rmani@nets.eu>
2022-09-29 09:58:43 +0200

    updated main class with annotations

--- src/main/java/net/guides/springboot2/azure/demo/Springboot2WebappJspApplication.java
+++ src/main/java/net/guides/springboot2/azure/demo/Springboot2WebappJspApplication.java
@@ -1,11 +1,15 @@
 package net.guides.springboot2.azure.demo;
 
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan(basePackages= {"net.guides.springboot2.azure.demo"}) 
+@EnableAutoConfiguration
 public class Springboot2WebappJspApplication {
 
 	public static void main(String[] args) {
